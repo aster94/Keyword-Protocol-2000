14d6c8fd352ca4c692a6f2be2caf8e57014eb5be
Vincenzo Gibiino <enzo.gib@gmail.com>
2018-05-15 00:43:16 +0200

    test

---
+++ src/pid.h
@@ -0,0 +1,14 @@
+//pids
+
+#if defined (GSXR_600_2011)
+	#define PROVA 69
+	#define PROVA2 99
+	
+
+#elif defined(GSXR_750_2011) 
+	#define PROVA 10
+	#define PROVA2 56
+
+#else
+	#error "no bike selected"
+#endif
